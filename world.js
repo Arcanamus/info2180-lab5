78d8a55b402a1b989cdcbcf6f2ea3c8d81a51885
Jevon Forrest <jevonforrest77@gmail.com>
2021-11-07 19:10:37 -0500

    Table update

--- world.js
+++ world.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', function(){
           var xhr = new XMLHttpRequest();
 
           xhr.onreadystatechange = function(){
-          if(xhr.readyState===XMLHttpRequest.DONE && xhr.status===200){
-            document.getElementById("result").innerHTML=xhr.responseText;
+          if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+            document.getElementById("result").innerHTML = xhr.responseText;
           }
-          if( xhr.readyState===XMLHttpRequest.DONE && xhr.status===404){
-            alert("ERROR:");
+          if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 404){
+            alert("ERROR: " + xhr.status);
           }
         }
             xhr.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value), true);
--- world.php
+++ world.php
@@ -10,12 +10,29 @@ if($_SERVER['REQUEST_METHOD'] === 'GET' ){
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Country Name</th>";
+    echo "<th>Continent</th>";
+    echo "<th>Independence Year</th>";
+    echo "<th>Head of State</th>";
+    echo "</tr>";
+    foreach($results as $row) {
+      $countryName = $row['name'];
+      $continent = $row['continent'];
+      $independenceYear = $row['independence_year'];
+      $headOfState = $row['head_of_state'];
+      echo "<tr>";
+      echo "<td>$countryName</td>";
+      echo "<td>$continent</td>";
+      echo "<td>$independenceYear</td>";
+      echo "<td>$headOfState</td>";
+      echo "</tr>";
+    }
+    echo "</table>";
   }
 }
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
